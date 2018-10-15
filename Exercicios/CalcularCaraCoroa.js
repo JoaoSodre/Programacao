5cbb9e028f313d53ca685e0dca92049873f8d8fa
João Victor S <40473133+JoaoSodre@users.noreply.github.com>
2018-10-14 22:46:54 -0300

    Update CalcularCaraCoroa.js

--- Exercicios/CalcularCaraCoroa.js
+++ Exercicios/CalcularCaraCoroa.js
@@ -50,4 +50,72 @@ function CalcularCaraCoroa(quantasVezes)
 
     alert("Quantidades de Caras: " + quantidadeCaras.length);
     alert("Quantidades de Coroas: " + quantidadeCoroas.length);
+    // return resultados;
 }
+/*
+function Repetiu() 
+{
+    PPP = resultados[YY + 1];
+    return PPP;
+}
+
+var duasCoroaSeg = [];
+var duasCaraSeg = [];
+var tresCoroaSeg = [];
+var tresCaraSeg = [];
+resultados = CalcularCaraCoroa(5);
+
+for (var YY = 0; YY < resultados.length; YY++)
+{
+    PPP = resultados[YY]
+    
+    if(PPP == "Coroa")
+    {
+        YY++;
+		Repetiu();
+        if (PPP == "Coroa")
+        {
+			YY++;
+            Repetiu();
+            if (PPP == "Coroa")
+            {
+                tresCaraSeg.push(1)
+                continue;
+            }
+            else 
+            {
+				YY++;
+                duasCaraSeg.push(1);
+                continue;
+            }
+        }
+        else 
+		{YY++; 
+		continue;}
+    }
+    
+    if(PPP == "Cara")
+    {
+        Repetiu();
+        if (PPP == "Cara")
+        {
+            Repetiu();
+            if (PPP == "Cara")
+            {
+                tresCoroaSeg.push(1)
+                continue;
+            }
+            else 
+            {
+                duasCoroaSeg.push(1);
+                continue;
+            }
+        }
+        else {continue;}
+    }
+}
+console.log("2 Coroas: " + duasCoroaSeg.length);
+console.log("2 Caras: " + duasCaraSeg.length);
+console.log("3 Coroas: " + tresCoroaSeg.length);
+console.log("3 Caras: " + tresCaraSeg.length);
+*/
